422a505bd3f98c55b6ae0a757e0fdeb088ba03ae
Anasvirat <anasmohammed18745@gmail.com>
2025-05-13 18:40:28 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+
+const streams = {
+  '18456': 'https://anasvirat.github.io/PHP/513.m3u8',
+  '18457': 'https://anasvirat.github.io/PHP/500.m3u8'
+};
+
+app.get('/ipl92.m3u8', (req, res) => {
+  const id = req.query.id;
+  const streamUrl = streams[id];
+
+  if (streamUrl) {
+    request.get(streamUrl).pipe(res);
+  } else {
+    res.status(404).send('#EXTM3U\n# Stream Not Found');
+  }
+});
+
+app.listen(3000, () => console.log('Server running'));
